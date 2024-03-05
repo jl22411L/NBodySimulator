2d28691b6aecd51777c3e7524663ecee0d28557b
jason <jason2richards@gmail.com>
2024-03-05 23:39:41 +0000

    [NBS-15_create_a_satellite_module] Adding tracking of GUtilities DataStruct directories

---
+++ SourceCode/GenericLibraries/GUtilities/DataStructs/GUtilities_stateStruct.c
@@ -0,0 +1,22 @@
+/*
+ *    @File:         GUtilities_stateStruct.c
+ *
+ *    @ Brief:       Declares the global struct Utilities
+ *
+ *    @ Date:        04/03/2024
+ *
+ */
+
+/* Function Includes */
+/* None */
+
+/* Structure Include */
+#include "GUtilities/DataStructs/GUtilities_StateStructs.h"
+
+/* Data include */
+/* None */
+
+/* Generic Libraries */
+#include "GConst/GConst.h"
+
+GUtilities Utilities;
