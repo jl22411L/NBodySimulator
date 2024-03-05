fb2f13ee24f178b3d87cfee7cbadbc40922fea40
jason <jason2richards@gmail.com>
2024-03-05 23:34:19 +0000

    [NBS-15_create_a_satellite_module] Including GArchive_close

--- SourceCode/GenericLibraries/GArchive/PublicFunctions/GArchive_close.c
+++ SourceCode/GenericLibraries/GArchive/PublicFunctions/GArchive_close.c
@@ -23,51 +23,55 @@
 /* Generic Libraries */
 #include "GConst/GConst.h"
 #include "GZero/GZero.h"
+#include "GLog/GLog.h"
 
 int GArchive_close(GArchive *p_archive_in)
 {
   /* Declare local variables */
   char    buffer[GARCHIVE_MAX_BUFFER];
+  FILE   *p_configFile;
   uint8_t i;
 
   /* Clear buffer */
   GZero(buffer, char[GARCHIVE_MAX_BUFFER]);
+  p_configFile = NULL;
 
   /* Close the file of the data */
   fclose(p_archive_in->p_archiveFolder);
 
   /* Create string to metadata file */
-  strncat(p_archive_in->p_archiveDataFilename, "/Config.ini", 11);
+  sprintf(buffer, "%s/Config.ini", p_archive_in->p_archiveDataFilename);
 
   /* Open meta data ini file */
-  p_archive_in->p_archiveFolder =
-      fopen(p_archive_in->p_archiveDataFilename, "w");
+  p_configFile = fopen(buffer, "w");
+
+  /* Check to make sure file open correctly */
+  if(p_configFile == NULL)
+  {
+    GError("File did not open correctly");
+  }
 
   /* Write Section Name */
-  fprintf(p_archive_in->p_archiveFolder, "[Config]\n");
+  fprintf(p_configFile, "[Config]\n");
 
   /* Write the simTime member to config file */
-  fprintf(p_archive_in->p_archiveFolder, "col[0] = simTime_s\n");
+  fprintf(p_configFile, "col[0] = simTime_s\n");
 
   /* Write keys of ini file */
   for (i = 1; i < p_archive_in->nCols + 1; i++)
   {
     /* Write key and value to config file */
-    fprintf(
-        p_archive_in->p_archiveFolder,
-        "col[%d] = %s \n",
-        i,
-        p_archive_in->colName[i - 1]);
+    fprintf(p_configFile, "col[%d] = %s \n", i, p_archive_in->colName[i - 1]);
   }
 
   /* Add new line to bottom of file */
-  fprintf(p_archive_in->p_archiveFolder, "\n");
+  fprintf(p_configFile, "\n");
 
   /* Close config file */
-  fclose(p_archive_in->p_archiveFolder);
+  fclose(p_configFile);
 
   /* Free memory from heap */
-  free(&p_archive_in->p_archiveDataFilename);
+  free(p_archive_in->p_archiveDataFilename);
 
   /* Clear archive */
   GZero(p_archive_in, GArchive);
